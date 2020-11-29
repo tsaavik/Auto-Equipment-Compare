db1d8cfb72d47f04111a05c36ed68a8d7b82b91d
David Mcanulty <github@hellspark.com>
2020-11-29 12:16:03 -0800

    added console command

--- consolecommand.lua
+++ consolecommand.lua
@@ -3,17 +3,17 @@ EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 EventFrame:SetScript("OnEvent", function(self, event, ...)
    if(event == "PLAYER_ENTERING_WORLD") then
-     print("/console set alwaysCompareItems 1")
+     ConsoleExec( "set alwaysCompareItems 1" );
    end
 end)
 
 -- Slash command to enable/disable, mostly for testing
 local function MyAddonCommands(msg, editbox)
   if msg == 'on' then
-    print("/console set alwaysCompareItems 1")
+    ConsoleExec( "set alwaysCompareItems 1" );
     print("Auto Compare active")
   else
-    print("/console set alwaysCompareItems 0")
+    ConsoleExec( "set alwaysCompareItems 0" );
     print("Auto Compare deactived, run again with on to re-enable")
   end
 end
