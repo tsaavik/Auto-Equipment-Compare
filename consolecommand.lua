5378c72e8413ab074f6e4844d85de5de4883c0f3
David Mcanulty <github@hellspark.com>
2020-11-29 12:42:56 -0800

    fixed lua syntax

--- consolecommand.lua
+++ consolecommand.lua
@@ -1,23 +1,26 @@
+-- https://rextester.com/l/lua_online_compiler
+--
 local EventFrame = CreateFrame("frame", "EventFrame")
 EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 EventFrame:SetScript("OnEvent", function(self, event, ...)
    if(event == "PLAYER_ENTERING_WORLD") then
-     ConsoleExec( "set alwaysCompareItems 1" );
+      print("Auto Compare actived")
+      ConsoleExec( "set alwaysCompareItems 1" );
+      active = true
    end
 end)
 
 -- Slash command to enable/disable, mostly for testing
-local function MyAddonCommands(msg, editbox)
-  if msg == 'on' then
-    ConsoleExec( "set alwaysCompareItems 1" );
-    print("Auto Compare active")
-  else
-    ConsoleExec( "set alwaysCompareItems 0" );
-    print("Auto Compare deactived, run again with on to re-enable")
-  end
+SLASH_COMPARE1 = "/compare"
+function SlashCmdList.COMPARE(msg, editbox)
+   if(active == true) then
+      ConsoleExec( "set alwaysCompareItems 0" );
+      print("Auto Compare deactivated, run again to enable")
+      active = false
+   else
+      ConsoleExec( "set alwaysCompareItems 1" );
+      print("Auto Compare activated")
+      active = true
+   end
 end
-
-SLASH_COMPARE = '/compare'
-SlashCmdList["COMPARE"] = MyAddonCommands
-
